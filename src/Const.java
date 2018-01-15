a7e238f00d0b747f3df5754b04b90c319f40f0a2
Cameron Jett <jett.18@osu.edu>
2018-01-15 12:00:29 -0500

    Moved test files into their own folder

--- src/Const.java
+++ src/Const.java
@@ -25,10 +25,10 @@ final class Const {
 
     static final String TEST_ENTRANCE_DIALOG = "test entrance";
 
-    static final String TEST_FILE_NAME = "src\\test game.txt";
-    static final String TEST_FILE_WITH_ROOMS_NAME = "src\\test game with rooms.txt";
-    static final String TEST_FILE_WITH_PEOPLE_NAME = "src\\test game with people.txt";
-    static final String TEST_FILE_WITH_ITEMS_AND_HIDDEN_DIALOG = "src\\test game with people items and hidden dialog.txt";
+    static final String TEST_FILE_NAME = "src\\test game scripts\\test game.txt";
+    static final String TEST_FILE_WITH_ROOMS_NAME = "src\\test game scripts\\test game with rooms.txt";
+    static final String TEST_FILE_WITH_PEOPLE_NAME = "src\\test game scripts\\test game with people.txt";
+    static final String TEST_FILE_WITH_ITEMS_AND_HIDDEN_DIALOG = "src\\test game scripts\\test game with people items and hidden dialog.txt";
 
     //commands
     static final String MOVE = "move";
@@ -59,6 +59,6 @@ final class Const {
     static final Character NULL_CHAR = '\u0000';
 
     //------------------ Simple Game ------------------//
-    static final String SIMPLE_GAME_LOAD_FILE = "src\\SimpleGame.txt";
+    static final String SIMPLE_GAME_LOAD_FILE = "src\\test game scripts\\SimpleGame.txt";
 
 }
